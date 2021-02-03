dc0a67e4af92e874d0683688a3d1e6c32b83b6d4
Alex <sasha89253215539@yandex.ru>
2021-02-03 23:28:34 +0300

    main done

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -24,3 +24,57 @@ if ( headerCarouselItems.length < 10 ) {
 } else {
     headerDotsAll.textContent = headerCarouselItems.length
 }
+
+$(document).ready(function(){
+    $(".projects__carousel-first").owlCarousel({
+        margin: 50,
+        nav: false,
+        dots: false,
+        smartSpeed: 60000,
+        loop: true,
+        autoplay: true,
+        autoPlaySpeed: 60000,
+        autoPlayTimeout: 60000,
+        autoplayHoverPause: true,
+        lazyLoad:true,
+        autoWidth: true,
+    });
+});
+
+$(document).ready(function(){
+    $(".projects__carousel-second").owlCarousel({
+        margin: 50,
+        nav: false,
+        dots: false,
+        smartSpeed: 60000,
+        loop: true,
+        autoplay: true,
+        autoPlaySpeed: 60000,
+        autoPlayTimeout: 60000,
+        autoplayHoverPause: true,
+        lazyLoad:true,
+        autoWidth: true,
+        rtl:true,
+    });
+});
+
+$(document).ready(function(){
+    $(".quote__carousel").owlCarousel({
+        items: 1,
+        nav: false,
+        dots: false,
+        mouseDrag: false,
+        smartSpeed: 3000,
+        loop: true
+    });
+});
+
+var owlQuote = $('.quote__carousel');
+
+$('.quote__nav-btn-right').click(function() {
+    owlQuote.trigger('next.owl.carousel');
+})
+
+$('.quote__nav-btn-left').click(function() {
+    owlQuote.trigger('prev.owl.carousel');
+})
